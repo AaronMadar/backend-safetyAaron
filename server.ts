1020a2e70ce2d15f6166e56d61a500561a92ee3b
Aaron <aaronmadar2655@gmail.com>
2025-12-14 16:08:28 +0200

    cors

--- server.ts
+++ server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import { AppDataSource } from './data-source'; 
 import safetyEventRouter from '@/routes/safety-event-route';
+import cors from 'cors';
 
 const server = express();
 const PORT = 3000;
 
+server.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 server.use(express.json());
 server.use('/safety-event', safetyEventRouter);
-
 
 
 // --- Le démarrage du serveur dépend de la connexion BDD ---
